19023b7c393d730a9b89d1b6077d7a64b1f6e9a7
leslientodi <lesliehazard500@gmail.com>
2024-01-03 12:08:52 +0000

    completed html

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function InfoPage (){
   return (
     <>
+      <img src='../src/images/personal.jpeg'></img>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Nana Ntodi</h2>
+        <p>Frontend Developer</p>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <button>Email</button>
+        <button>LinkedIn</button>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h3>About</h3>
+        <small>
+          Hey there! I'm the epitome of cool. With an effortless swagger,
+           I navigate life with confidence and charm.
+            My style game is always on point, rocking the latest trends with my own unique flair.
+             I've got this magnetic personality that draws people in 
+              cracking jokes, making them laugh, and leaving a lasting impression.
+        </small>
+        <h3>Interests</h3>
+        <small>
+          I approach challenges with a calm demeanor,
+           never letting stress get the best of me.
+            Open-minded and always up for new experiences, 
+            I thrive on adventure and inspire those around me to embrace the thrill of the unknown.
+             Life's too short not to be cool, and I've got that covered in every aspect.
+              Cheers to being effortlessly awesome!
+        </small>
+            
+      </div>
+      <footer>
+        <a><img src='../src/images/fb.jpeg'></img></a>
+        <a><img src='../src/images/insta.jpeg'></img></a>
+        <a><img src='../src/images/linkedin.jpeg'></img></a>
+        <a><img src='../src/images/whatsapp.jpeg'></img></a>
+
+      </footer>
+      
+
+
+
     </>
+    
   )
 }
 
-export default App
+export default InfoPage
